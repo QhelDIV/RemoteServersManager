755601f0549b6e1cd5ff7694e552278a14acd6bb
qheldiv <yxgunbreakable@gmail.com>
2018-07-24 10:36:12 +0800

    add README.md and .gitignore

---
+++ README.md
@@ -0,0 +1,6 @@
+# Remote Servers Manager
+A convenient tool to automatically manage and maintain multiple remote servers.
+Usage: create your own operation class like the class in UserCreater.py and a remote\*.py file then python3 remote\*.py
+# configuration
+You can configure the hosts you want to control and the login user and password in config.py.
+You can check example_config.py for detail.
--- remoteAddUser.py
+++ remoteAddUser.py
@@ -3,15 +3,6 @@ from UserCreater import *
 from config import *
 from remoteControl import *
 
-command = "ls"
-
-port = 22
-def apiWraper(api):
-    def wraped(arg):
-        stdin, stdout, stderr = api(arg)
-        print(stdout.read().decode('utf'))
-        print(stderr.read().decode('utf'))
-    return wraped
 remoteController = RemoteControl(UserCreater)
 for newUserInfo in newUserInfos:
     newuser = newUserInfo[0]
